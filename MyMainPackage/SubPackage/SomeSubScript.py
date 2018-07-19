b5d8be88e992e7dafe564993aaa53cb0afaeae18
Christopher Pace <Christopher_pace@ymail.com>
2018-07-19 15:15:11 -0500

    Initial Commit of All Files

---
+++ MyMainPackage/SubPackage/SomeSubScript.py
@@ -0,0 +1,2 @@
+def function2():
+    print("This is the sub module, not the main!")
---
+++ Test.py
@@ -0,0 +1,5 @@
+from MyMainPackage import SomeMainScript
+from MyMainPackage.SubPackage import SomeSubScript
+
+SomeMainScript.function1()
+SomeSubScript.function2()
